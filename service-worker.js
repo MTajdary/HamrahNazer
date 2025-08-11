92c3a75f3264e4abe63d3ba4adf123c4f0807700
MTajdary <mohammadtajdariii@gmail.com>
2025-08-11 11:54:01 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('hn-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('hn-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
